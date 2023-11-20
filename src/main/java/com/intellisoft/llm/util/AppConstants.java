ff5e08eda85c70ea9c61ad944bd044692e23850a
nelsonkimaiga <nelsonkimaiga@aol.com>
2023-11-20 14:21:06 +0300

    API keys change

--- src/main/java/com/intellisoft/llm/util/AppConstants.java
+++ src/main/java/com/intellisoft/llm/util/AppConstants.java
@@ -9,8 +9,8 @@ public class AppConstants {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
     public static final String API_KEY1 = "sk-";
-    public static final String API_KEY2 = "ASMuM2iFxws7kDKHdHyzT3";
-    public static final String API_KEY3 = "BlbkFJCZy5mmucccsp1TyUcKsC";
+    public static final String API_KEY2 = "mAT1ThiNaWau9LL61IsZT";
+    public static final String API_KEY3 = "3BlbkFJcrNZZh3OIk0AvYVabNxd";
     public static final String MODEL = "gpt-3.5-turbo";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
